a2cb8606b23f47231c5dfb5a705c5d0c1e0008db
Ananya Sristy <ansristy@gmail.com>
2025-10-06 10:36:10 -0400

    block24

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
+import EpisodeList from "../episodes/EpisodeList";
+import EpisodeDetails from "../episodes/EpisodeDetails";
 import "./shows.css";
-
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+export default function ShowDetails({ show }) {
+  const [selectedEpisode, setSelectedEpisode] = useState(null);
+  if (!show) {
+    return <div className="show-details">Select a show to see details.</div>;
+  }
+  return (
+    <div className="show-details">
+      <EpisodeList episodes={show.episodes} setSelectedEpisode={setSelectedEpisode}/>
+      <EpisodeDetails episode={selectedEpisode}/>
+    </div>
+  );
 }
--- src/shows/ShowSelection.jsx
+++ src/shows/ShowSelection.jsx
@@ -1,6 +1,17 @@
 import "./shows.css";
-
 /** A navbar that allows users to choose between a list of shows */
-export default function ShowSelection() {
-  return <nav className="shows"></nav>;
+export default function ShowSelection({shows,setSelectedShow, selectedShow}) {
+  return (
+    <nav className="shows">
+      {shows.map((show) => (
+        <a
+          key={show.name}
+          className={`show ${selectedShow?.name === show.name ? "selected" : ""}`}
+          onClick={() => setSelectedShow(show)}
+        >
+          {show.name}
+        </a>
+      ))}
+    </nav>
+  );
 }
